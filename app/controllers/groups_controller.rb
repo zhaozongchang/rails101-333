465f47b43a727e45bf42c22ce9fdb1ff008ec72e
zhaozongchang <xingyingsuifeng@gmail.com>
2018-07-26 20:30:23 +0800

    implement group#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,10 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
@@ -13,9 +17,19 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "更新成功"
+  end
+
   private
 
   def group_params
     params.require(:group).permit(:title, :description)
-  end 
+  end
 end
