767e23ca4f415e455e02e3c28cc9d362e1da41c2
zhaozongchang <xingyingsuifeng@gmail.com>
2018-07-27 07:21:59 +0800

    install devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725071527) do
+ActiveRecord::Schema.define(version: 20180726134740) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
