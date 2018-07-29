037ba0c8314fe6a27a145da0e0103753651d5f55
zhaozongchang <xingyingsuifeng@gmail.com>
2018-07-29 08:28:25 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :desctroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :desctroy, :join, :quit]
 
   def index
     @groups = Group.all
@@ -43,6 +43,29 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, alert: "删除成功"
   end
+
+   def join
+     @group = Group.find(params[:id])
+
+     if !current_user.is_member_of?(@group)
+       current_user.join!(@group)
+       flash[:notice] = "加入本讨论版成功"
+     else
+       flash[:warning] = "你已经是本讨论版成员了"
+     end
+
+     def quit
+       @group = Group.find(params[:id])
+
+       if current_user.is_member_of?(@group)
+         current_user.quti!(@group)
+         flash[:alert] = "已退出本讨论版"
+       else
+         flash[:warning] = "你不是本讨论版成员，无法退出"
+       end
+       redirect_to group_path(@group)
+     end
+   end
 
   private
 
