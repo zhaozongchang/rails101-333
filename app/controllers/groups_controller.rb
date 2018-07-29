48759eee2405a1d95abf7707eb37ad68db829a2f
zhaozongchang <xingyingsuifeng@gmail.com>
2018-07-29 08:39:58 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,6 +20,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
     redirect_to groups_path
   else
     render :new
@@ -44,27 +45,30 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "删除成功"
   end
 
-   def join
-     @group = Group.find(params[:id])
+  def join
+    @group = Group.find(params[:id])
 
      if !current_user.is_member_of?(@group)
        current_user.join!(@group)
-       flash[:notice] = "加入本讨论版成功"
+       flash[:notice] = "加入本讨论版成功！"
      else
-       flash[:warning] = "你已经是本讨论版成员了"
+       flash[:warning] = "你已经是本讨论版成员了！"
      end
 
-     def quit
-       @group = Group.find(params[:id])
+     redirect_to group_path(@group)
+   end
 
-       if current_user.is_member_of?(@group)
-         current_user.quti!(@group)
-         flash[:alert] = "已退出本讨论版"
-       else
-         flash[:warning] = "你不是本讨论版成员，无法退出"
-       end
-       redirect_to group_path(@group)
+   def quit
+     @group = Group.find(params[:id])
+
+     if current_user.is_member_of?(@group)
+       current_user.quit!(@group)
+       flash[:alert] = "已退出本讨论版！"
+     else
+       flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
      end
+
+     redirect_to group_path(@group)
    end
 
   private
